3d0cd8589274f5315a99483aafef9c7e3d980e36
Riyoo <40727557+PavanPavu@users.noreply.github.com>
2025-04-02 00:14:05 +0100

    Update models.py
    URL to Text

--- library_app/models.py
+++ library_app/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     quantity = models.PositiveIntegerField()
 
-    cover_url = models.URLField(blank=True, null=True)
+    cover_url = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
